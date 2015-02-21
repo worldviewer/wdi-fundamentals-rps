4235b424e13d237fde907cab82d96b1a31ecd746
Chris Reeve <paradigmsareconstructed@gmail.com>
2015-02-21 14:50:58 -0800

    Conditional code to test computer vs player

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -40,7 +40,23 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+
+    if (playerMove === computerMove) {
+        winner = 'tie';
+    } else if ((playerMove == 'rock') && (computerMove === 'scissors')) {
+        winner = 'player';
+    } else if ((playerMove == 'rock') && (computerMove === 'paper')) {
+        winner = 'computer';
+    } else if ((playerMove == 'scissors') && (computerMove === 'rock')) {
+        winner = 'computer';
+    } else if ((playerMove == 'scissors') && (computerMove === 'paper')) {
+        winner = 'player';
+    } else if ((playerMove == 'paper') && (computerMove === 'rock')) {
+        winner = 'player';
+    } else if ((playerMove == 'paper') && (computerMove === 'scissors')) {
+        winner = 'computer';
+    }
+
     return winner;
 }
 
